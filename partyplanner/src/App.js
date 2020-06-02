c4e705496997e3245a17e5bd4eedd7432ebc34a0
chrissie-akau <cakau9@gmail.com>
2020-06-01 17:35:33 -0700

    Yeah

--- partyplanner/src/App.js
+++ partyplanner/src/App.js
@@ -1,33 +1,59 @@
-import React {Component} from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-class App extends Component{
+class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      hamburger: 0,
-      hot_dog: 0, 
-      pizza: 0,
-      cake: 0,
-      chips: 0,
-      veggie: 0,
-      soda_bottle: 0,
-      plates: 0,
-      utensils: 0, 
-      cups: 0, 
-      napkins: 0, 
-      tables: 0,
-      chairs: 0,
-      guest_input: null,
-      space_input: null,
+      guest_input: "",
+      venue_size_input: "",
+      list: ""
     }
   }
-}
+  getList = (number) => {
+  const{ guest_input }= this.state
+   if (number > 0){
+     this.setState({ list: `${Math.ceil(guest_input/3)} soda, ${Math.ceil(guest_input/4)} pizzas, ${guest_input} hamburgers, ${guest_input*5} weeny hottie dogs, ${Math.ceil(guest_input/12)} cake, ${Math.ceil(guest_input/4)} chips, ${Math.ceil(guest_input/4)} veggies, ${guest_input} plates, ${guest_input} forks, ${guest_input} spoons, ${guest_input} knives, ${guest_input} cups, ${guest_input} napkins, ${Math.ceil(guest_input/6+2)} tables, ${guest_input} chairs, ${Math.ceil(guest_input/2)} llamas and donkeys (rideable), ${guest_input*3} balloons, ${Math.ceil(guest_input/2)} packs of streamers.  Your supply budget is approximately ${guest_input*30} dollars!  You venue budget is ${guest_input*100}`})
+   }else{
+     this.setState({ list: "Please Put In a Number!"})
+   }
+  
+  }
 
-function App() {
-  return (
+  handleChange = (e) => {
+    this.setState({ guest_input: e.target.value })
+  }
+  handleSubmit = () =>{
+    const { guest_input } = this.state
+    const list = this.getList(guest_input)
+  }
+  render(){
+    return (
+      <div>
+        <h1>Party Planning? Sure! We Got You!</h1>
+        <h2>Enter Number Of Guests Below</h2>
+        <input
+          id="inputBox"
+          type='text'
+          onChange={ this.handleChange }
+        />
+        <br />
+        <h2>Enter Square Footage</h2>
+        <p>Living Room = 300 sq feet, Club House = 1000 sq feet, Ballroom = 3000 sq feet</p>
+        <input
+          id="inputBox"
+          type='text'
+        />
+        <br />
+        <button onClick={ this.handleSubmit }>
+          Enter
+        </button>
+        <p> { this.state.list } </p>
+      </div>
+    )
+  }
+}
     
-  );
-}
+  
 
-export default App;
+export default App
